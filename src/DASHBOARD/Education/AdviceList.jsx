fad09bd2cef4d5a692ebc5e0e3c4728b6febbe94
Prexidazie Morara <moraraprexidazie@gmail.com>
2026-01-31 14:20:54 +0300

    Advicecard.jsx

---
+++ src/DASHBOARD/Education/AdviceList.jsx
@@ -0,0 +1,44 @@
+//Collection of Advice Cards
+/*
+What it is
+
+A container component that:
+
+Holds an array of advice
+
+Loops through the array
+
+Renders multiple AdviceCards
+
+What it contains
+
+Advice data (array)
+
+.map() logic
+
+Layout for multiple cards
+*/ 
+
+/*import AdviceCard from "./AdviceCard";
+
+const AdviceList = () => {
+  const adviceTips = [
+    "Drink enough water daily.",
+    "Reduce salt intake.",
+    "Take medication at the same time every day.",
+  ];
+
+  return (
+    <div className="advice-list">
+      {adviceTips.map((tip, index) => (
+        <AdviceCard key={index} advice={tip} />
+      ))}
+    </div>
+  );
+};
+
+export default AdviceList;
+ */
+
+//array and mapping logic.
+
--- src/DASHBOARD/Medication/MedzSchedule.jsx
+++ src/DASHBOARD/Medication/MedzSchedule.jsx
@@ -1,1 +1,61 @@
 //(Optional) Allows adding/editing medication schedules
+//Planning the Medication schedule component
+// Acontainer component that;
+/*
+1. Adds medications
+2.Sets time(s) and frequency
+3.Views their medication plan
+
+What it entails
+
+Input fields:
+1.Medication name
+2.Dosage
+3.Time (morning / evening / custom time)
+4.Save button
+5.Stores data in state or context
+ */  
+
+/*import { useState } from "react";
+
+const MedicationSchedule = () => {
+  const [meds, setMeds] = useState([]);
+  const [name, setName] = useState("");
+  const [time, setTime] = useState("");
+
+  const addMedication = () => {
+    setMeds([...meds, { name, time }]);
+    setName("");
+    setTime("");
+  };
+
+  return (
+    <div>
+      <h3>Medication Schedule</h3>
+
+      <input
+        placeholder="Medication Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <input
+        type="time"
+        value={time}
+        onChange={(e) => setTime(e.target.value)}
+      />
+
+      <button onClick={addMedication}>Save</button>
+
+      {meds.map((med, index) => (
+        <p key={index}>
+          {med.name} at {med.time}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default MedicationSchedule;
+ */
+
